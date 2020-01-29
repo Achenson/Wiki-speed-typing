da0fa7aab132e30b5a182b4e82f980761c4f2be9
achenson <45999022+Achenson@users.noreply.github.com>
2020-01-29 16:12:05 +0100

    start

--- src/App.js
+++ src/App.js
@@ -5,13 +5,31 @@ function App() {
   return (
     <div className="App">
       <div className="outer-container">
+        <h3 className="title">Typing App</h3>
         <div className="main-square">
-    test
+          <div className="counter">
+          01:00
+          </div>
+          <div className="typing-display wiki-display">sfsdfsd</div>
+          <div className="typing-display current-display">sfsdfsd</div>
+
+          <div className="control-buttons">
+          
+          <button className="btn-control">Start</button>
+          <button className="btn-control">Pause</button>
+          <button className="btn-control">Reset</button>
+
+
+          </div>
+
 
         </div>
       </div>
     </div>
   );
 }
+
+
+
 
 export default App;
