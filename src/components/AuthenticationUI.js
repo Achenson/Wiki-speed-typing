2dbfb67a8ede3ed6cf3506f72a22059c8b1d4745
achenson <45999022+Achenson@users.noreply.github.com>
2020-04-06 10:41:09 +0200

    authenticationUI UI finished

--- src/components/AuthenticationUI.js
+++ src/components/AuthenticationUI.js
@@ -1,14 +1,30 @@
 import React from "react";
 
-
 function AuthenticationUI() {
 
-    return (
+  let isAuthenticated = true;
 
+  function authLinks() {
+    if (!isAuthenticated) {
+      return (
         <div className="auth-div">
-            <a className="auth-link">Register</a><a className="auth-link">Login</a>
+          <a className="auth-link">Register</a>
+          <a className="auth-link">Login</a>
         </div>
-    )
+      );
+    } else {
+      return (
+        <div className="auth-div">
+          <a className="auth-link">Stats</a>
+          <a className="auth-link">Logout</a>
+        </div>
+      );
+    }
+  }
+
+  return (
+     authLinks()
+  );
 }
 
 export default AuthenticationUI;
