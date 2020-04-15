55585969a759a4ce7332c2f12c10b006f297315a
achenson <45999022+Achenson@users.noreply.github.com>
2020-04-15 11:12:48 +0200

    Hints - charBar clickable

--- src/components/Hints.js
+++ src/components/Hints.js
@@ -7,15 +7,19 @@ import { faChartBar } from "@fortawesome/free-solid-svg-icons";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-function Hints({toggleStats, areHintsVisible, isAuthenticated, notification_true}) {
-
+function Hints({
+  toggleStats,
+  areHintsVisible,
+  isAuthenticated,
+  notification_true,
+}) {
   let history = useHistory();
 
   return (
     <div
       className="hints"
       style={{
-        visibility: `${areHintsVisible ? "visible" : "hidden"}`
+        visibility: `${areHintsVisible ? "visible" : "hidden"}`,
       }}
     >
       <div className="inner-hints container">
@@ -31,42 +35,37 @@ function Hints({toggleStats, areHintsVisible, isAuthenticated, notification_true
           </li>
           <li>Click on the article title to visit wikipedia page</li>
           <li>Mouse over results for more information</li>
-          <li>Register to access your top score &nbsp;  
-          <FontAwesomeIcon
-        icon={faChartBar}
-        size="1x"
-        
-        onClick={() => {
-        toggleStats();
+          <li>
+            Register to access your top score &nbsp;
+            <FontAwesomeIcon
+              icon={faChartBar}
+              size="1x"
+              onClick={() => {
+                toggleStats();
 
-          if (!isAuthenticated) {
-            notification_true();
-            history.push("/login");
-          }
-        }}
-      />
-</li>
+                if (!isAuthenticated) {
+                  notification_true();
+                  history.push("/login");
+                }
+              }}
+            />
+          </li>
         </ul>
       </div>
     </div>
   );
 }
 
-
-
 const mapStateToProps = (state) => {
   return {
     isAuthenticated: state.authState.isAuthenticated,
-   
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    
     notification_true: () => dispatch({ type: "NOTIFICATION_TRUE" }),
-    
-    
+  };
 };
 
 export default connect(
@@ -74,7 +73,3 @@ export default connect(
   mapDispatchToProps
   // Your component will receive dispatch by default, i.e., when you do not supply a second parameter to connect():
 )(Hints);
-
-
-
-
